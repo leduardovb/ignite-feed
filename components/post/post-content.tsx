ad8d64b0ba4eb1303ae1fea9b08ccbf3444447c2
leduardovb <luizbarreto@viptech.com.br>
2024-09-22 01:29:49 -0300

    feat(post-content): highlight post content

--- components/post/post-content.tsx
+++ components/post/post-content.tsx
@@ -2,6 +2,24 @@ interface Props {
   content: string
 }
 
+const regex = /(#\w+)/g
+
 export function PostContent({ content }: Props) {
-  return <div className="whitespace-break-spaces text-gray-400">{content}</div>
+  const highlight = () => {
+    const parts = content.split(regex)
+    return parts.map((part, index) => {
+      if (part.match(regex)) {
+        return (
+          <span key={index} className="font-bold text-primary">
+            {part}
+          </span>
+        )
+      }
+      return <span key={index}>{part}</span>
+    })
+  }
+
+  return (
+    <div className="whitespace-break-spaces text-gray-400">{highlight()}</div>
+  )
 }
